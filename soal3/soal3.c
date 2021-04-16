70da5fb01407441fa87f563ad52372b1a9b5add3
mirzaq19 <mirzaqarjap@gmail.com>
2021-04-16 16:58:12 +0700

    Soal3d,3e

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -7,58 +7,75 @@
 #include <unistd.h>
 #include <syslog.h>
 #include <string.h>
-#include <stdbool.h>
-#include <wait.h>
 #include <time.h>
+#include <wait.h>
 
-FILE *folderlog = NULL;
+
+//Soal 3D: Membuat program bash killer untuk menghentikan proses
+void makekiller(int argc,char **argv,int pid){
+    FILE *pkiller = fopen("killer.sh", "w");
+
+    if(strcmp(argv[1], "-x") == 0){ // Soal 3E: Menghentikan proses namun membiarkan proses selesai
+        fprintf(pkiller, "#!/bin/bash\nkill %d\nrm \"$0\"", pid);
+    }else if(strcmp(argv[1], "-z") == 0){ //Soal 3E: Menghentikan proses paksa
+        fprintf(pkiller, "#!/bin/bash\nkillall -9 soal3\nrm \"$0\"");
+    }else{
+        fprintf(pkiller, "#!/bin/bash\nkillall -9 soal3\nrm \"$0\"");
+    }
+    fclose(pkiller);
+
+    if(fork() == 0){
+        char *chomdarg[] = {"chmod", "+x", "killer.sh", NULL};
+        execv("/bin/chmod", chomdarg);
+    }
+}
 
 void caesarchiper(char* rawstr,int key){
     char ch;
     int i;
-    for (i = 0; rawstr[i] != '\0'; ++i){
+    for (i=0;rawstr[i]!='\0';i++){
         ch = rawstr[i];
-
-        if (ch >= 'a' && ch <= 'z'){
-            ch = ch + key;
+        if(ch>='a'&&ch<='z'){
+            ch=ch+key;
             if (ch > 'z') ch = ch - 'z' + 'a' - 1;
             rawstr[i] = ch;
         }
-        else if (ch >= 'A' && ch <= 'Z'){
-            ch = ch + key;
+        else if(ch>='A'&&ch<='Z'){
+            ch=ch+key;
             if (ch > 'Z') ch = ch - 'Z' + 'A' - 1;
             rawstr[i] = ch;
         }
     }
 }
 
-int main(){
+int main(int argc, char **argv){
+    FILE *parentid,*folderlog;
     pid_t pid, sid; // Variabel untuk menyimpan PID
+    char buffer[100], namagambartemp[204], namafoldertemp[100], url[100];
+    time_t rawtime;
 
-    char buffer[50],namagambartemp[50],namafoldertemp[50],url[50];
 
     pid = fork(); // Menyimpan PID dari Child Process
 
-    time_t rawtime;
-    struct tm *curtime;
-
     /* Keluar saat fork gagal
-     * (nilai variabel pid < 0) 
-     */
-    if (pid < 0){
+  * (nilai variabel pid < 0) */
+    if (pid < 0)
+    {
         exit(EXIT_FAILURE);
     }
 
     /* Keluar saat fork berhasil
   * (nilai variabel pid adalah PID dari child process) */
-    if (pid > 0){
+    if (pid > 0)
+    {
         exit(EXIT_SUCCESS);
     }
 
     umask(0);
 
     sid = setsid();
-    if (sid < 0){
+    if (sid < 0)
+    {
         exit(EXIT_FAILURE);
     }
 
@@ -66,90 +83,88 @@ int main(){
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
 
-    if(pid==0){
-        //Mempersiapkan variabel
-        folderlog = fopen("FolderLog.txt", "w+");
-        pid_t p1,p2,p3;
-        int s1,i; 
-        long ukuran; //ukuran gambar
+    int i,ukuran,program_id;
+    pid_t p1,p2;
+    folderlog = fopen("folderlog.txt", "w+");
+    char pesandl[20] = "Download Success";
 
-        while (1){
-            //Pengosongan varibael
-            strcpy(namagambartemp,"");
-            strcpy(namafoldertemp,"");
-            strcpy(buffer,"");
-
-            //Membuat nama folder berdasarkan timestamp [YYYY-MM-dd_hh:mm:ss]
-            time(&rawtime);
-            strftime(buffer, 50, "%Y-%m-%d_%X", localtime(&rawtime));
-            strcpy(namafoldertemp,buffer);
-
-            p1=fork();
-            
-            if(p1<0) exit(0);
-            //Pembuatan Folder dari timestamp
-            if(p1==0){
-                char *argv[] = {"mkdir", "-p",namafoldertemp, NULL};
-                execv("/bin/mkdir", argv);
-            }
-            //Mendownload 10 gambar dan dimasukkan ke folder yang dibuat
-            else{
-                while ((wait(&s1)) > 0); //menunggu folder dibuat
-                chdir(namafoldertemp); //pindah direktori ke folder yang telah dibuat
-                fprintf(folderlog,"Status Folder : Folder %s berhasil dibuat\n",namafoldertemp);
-                fflush(folderlog);
-
-                for(i=0;i<10;i++){
-
-                    p2 = fork();
-
-                    //Membuat nama file gambar dan urlnya
-                    strcpy(buffer,"");
-                    time(&rawtime);
-                    strftime(buffer, 50, "%Y-%m-%d_%X", localtime(&rawtime));
-                    strcpy(namagambartemp,buffer);
-                    strcat(namagambartemp,".jpg");
-                    ukuran = (long)time(NULL);
-                    ukuran = (ukuran % 1000) + 50;
-                    sprintf(url,"https://picsum.photos/%ld",ukuran);
-
-                    //mendownload gambar
-                    if(p2==0){
-                        char *downloadarg[] = {"wget","-qO", namagambartemp,url, NULL};
-                        execv("/usr/bin/wget", downloadarg);
-                    }
-                    fprintf(folderlog,"status gambar : gambar %s, url %s berhasil diunduh\n",namagambartemp,url);
-                    fflush(folderlog);
-                    sleep(5);
-                }
-
-                //mmebuat file status download yang ternkripsi dengan caesar chiper dengan shift 5
-                char pesandownload[20] = "Download Success";
-                FILE* statusdownload = fopen("status.txt", "w+");
-                caesarchiper(pesandownload,5);
-                fprintf(statusdownload,"%s",pesandownload);
-                fclose(statusdownload);
-                fprintf(folderlog,"Status pesan : status.txt berhasil dibuat\n");
-                fflush(folderlog);
-
-                chdir("..");
-
-                p3 = fork();
-
-                //membuat zip dari folder yang sudah diisi 10 gambar
-                char namazip[100];
-                strcpy(namazip,namafoldertemp);
-                strcat(namazip,".zip");
-                if(p3==0){
-                    char *zipargv[] = {"zip", "-rm", namazip, namafoldertemp, NULL};
-                    execv("/usr/bin/zip", zipargv);
-                }
-                fprintf(folderlog,"Status Zip : File zip %s sudah berhasil dibuat\n",namazip);
-                fflush(folderlog);
-            }
-            sleep(40);
-        }
-        fclose(folderlog);
+    int ca;
+    for(ca=0;ca<argc;ca++){
+        fprintf(folderlog,"argv[%d] : %s\n",ca,argv[ca]);
     }
 
+    if(pid == 0){
+        makekiller(argc,argv,(int)getpid());
+    }
+
+    fprintf(folderlog,"Program Killer dibuat\n");
+
+    while (1){
+        int s1,s2;
+        //Pengosongan varibael
+        strcpy(namagambartemp,"");
+        strcpy(namafoldertemp,"");
+        strcpy(buffer,"");
+
+        //Membuat nama folder berdasarkan timestamp [YYYY-MM-dd_hh:mm:ss]
+        time(&rawtime);
+        strftime(buffer, 100, "%Y-%m-%d_%X", localtime(&rawtime));
+        strcpy(namafoldertemp,buffer);
+
+        p1 = fork();
+        
+        if(p1==0){ //Soal3A: Membuat direktori setiap 40 detik
+            char *mkdirarg[] = {"mkdir", "-p",namafoldertemp, NULL};
+            execv("/bin/mkdir", mkdirarg);
+        }
+        else{ //Soal 3B: Mendownload 10 gambar setiap 5 detik dengan ukuran time%1000+50
+            while((wait(&s1))>0);
+            fprintf(folderlog,"Folder %s berhasil dibuat\n",namafoldertemp);
+            fflush(folderlog);
+
+            for(i=0;i<10;i++){
+                time(&rawtime);
+                strftime(buffer, 50, "%Y-%m-%d_%X", localtime(&rawtime));
+                
+                sprintf(namagambartemp,"%s/%s.jpg",namafoldertemp,buffer);
+                ukuran = (int)time(NULL);
+                ukuran = (ukuran%1000)+50;
+                sprintf(url,"https://picsum.photos/%d",ukuran);
+
+                p2 = fork();
+                if(p2==0){
+                    char *dlarg[] = {"wget", "-qO", namagambartemp, url, NULL};
+                    execv("/usr/bin/wget", dlarg);
+                }
+                fprintf(folderlog,"Gambar %s berhasil diunduh\n",namagambartemp);
+                fflush(folderlog);
+                sleep(5);
+            }
+
+            //SOal 3C: Membuat status.txt yang dienkripsi di dalam folder setelah berhasil download semua gambar
+
+            char statusdl[112];
+            sprintf(statusdl,"%s/status.txt",namafoldertemp);
+
+            caesarchiper(pesandl,5);
+            FILE *isdownload = fopen(statusdl,"w");
+            fprintf(isdownload,"%s",pesandl);
+            fclose(isdownload);
+            fprintf(folderlog,"Status pesan : status.txt berhasil dibuat\n");
+            fflush(folderlog);
+
+            //Soal 3C: Membuat zip dari folder yang sudah diisi 10 gambar
+            char namazip[112];
+            sprintf(namazip,"%s.zip",namafoldertemp);
+            if(fork()==0){
+                char *zipargv[] = {"zip", "-rm", namazip, namafoldertemp, NULL};
+                execv("/usr/bin/zip", zipargv);
+            }
+            while((wait(&s2))>0);
+            fprintf(folderlog,"Status Zip : File zip %s sudah berhasil dibuat\n",namazip);
+            fflush(folderlog);
+        }
+        sleep(40); //Soal 3A: Direktori/40detik
+    }
+    fclose(folderlog);
 }
